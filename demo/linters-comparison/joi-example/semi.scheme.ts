4f5ac590c29cbfb4f79c5bfb84db9e9a1083eb68
vlashchanka <vlashchanka@evolutiongaming.com>
2020-09-20 12:26:58 +0300

    Make more compact version

--- demo/linters-comparison/joi-example/semi.scheme.ts
+++ demo/linters-comparison/joi-example/semi.scheme.ts
@@ -1,17 +1,8 @@
 import * as Joi from "joi";
 
 export const semi = Joi.object({
-    id: Joi
-        .string()
-        .required(),
-    name: Joi
-        .string()
-        .required(),
-    description: Joi
-        .string()
-        .optional(),
-    level: Joi
-        .string()
-        .required()
-        .valid('warn', 'error'),
+    id: Joi.string().required(),
+    name: Joi.string().required(),
+    description: Joi.string().optional(),
+    level: Joi.string().required().valid('warn', 'error'),
 });
