28af283cc1157a76a633648faba2441700ad0aad
vlashchanka <vlashchanka@evolutiongaming.com>
2020-09-20 11:53:04 +0300

    Add joi scheme update

--- demo/linters-comparison/joi-example/semi.scheme.ts
+++ demo/linters-comparison/joi-example/semi.scheme.ts
@@ -1,8 +1,17 @@
 import * as Joi from "joi";
 
 export const semi = Joi.object({
-    id: Joi.string().required(),
-    name: Joi.string().required(),
-    description: Joi.string().optional(),
-    level: Joi.string().valid('warn', 'error'),
+    id: Joi
+        .string()
+        .required(),
+    name: Joi
+        .string()
+        .required(),
+    description: Joi
+        .string()
+        .optional(),
+    level: Joi
+        .string()
+        .required()
+        .valid('warn', 'error'),
 });
